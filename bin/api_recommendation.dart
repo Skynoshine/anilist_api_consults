9754b3b185bf2fad302557022c4a77802e495bff
Skynoshine <gabrielrmello515@gmail.com>
2023-07-28 17:43:39 -0300

    add comparelist

--- bin/api_recommendation.dart
+++ bin/api_recommendation.dart
@@ -20,7 +20,7 @@ class ApiRecommendation {
       if (showTitles) {
         print('banners titles:${_titlesBanners.toString().toLowerCase()}');
       }
-      return _titlesBanners.toString().toLowerCase();
+      return _titlesBanners;
     } else {
       print('Falha na requisição: ${response.statusCode}');
       return [];
@@ -50,27 +50,34 @@ class ApiRecommendation {
         final titleRomaji = titleObject != null ? titleObject['romaji'] : null;
 
         if (titleEnglish != null) {
-          _titlesAnilist.add('englishTitle: $titleEnglish');
+          _titlesAnilist.add(titleEnglish);
         }
 
         if (titleRomaji != null) {
-          _titlesAnilist.add('RomajiTitle: $titleRomaji');
+          _titlesAnilist.add(titleRomaji);
         }
       }
       print('titles anilist:${_titlesAnilist.toString().toLowerCase()}');
-
-      Set<String> intersection =
-          _titlesAnilist.toSet().intersection(_titlesBanners.toSet());
-
-      if (intersection.isNotEmpty) {
-        print('As duas listas possuem pelo menos um título em comum:');
-        print(intersection);
-      } else {
-        print('As duas listas não possuem títulos em comum.');
-      }
       return _titlesAnilist.toString().toLowerCase();
     } else {
       print('Falha na requisição: ${_response.statusCode}');
+    }
+  }
+
+  Future<void> _compareListsTitles() async {
+    Set<String> setTitlesAnilist =
+        _titlesAnilist.map((title) => title.toLowerCase().trim()).toSet();
+    Set<String> setTitlesBanners =
+        _titlesBanners.map((title) => title.toLowerCase().trim()).toSet();
+
+    Set<String> titlesInCommon =
+        setTitlesAnilist.intersection(setTitlesBanners);
+
+    if (titlesInCommon.isNotEmpty) {
+      print('As duas listas têm os seguintes títulos em comum:');
+      print(titlesInCommon);
+    } else {
+      print('As duas listas não têm títulos em comum.');
     }
   }
 }
@@ -82,5 +89,7 @@ void main() async {
   await apiRecommendation._fetchTitlesFromBanners(false);
 
   // Aguarda recommendations para preencher _titlesAnilist
-  await apiRecommendation._fetchRecommendationsAnilist("Overlord");
+  await apiRecommendation._fetchRecommendationsAnilist("youjo senki");
+
+  await apiRecommendation._compareListsTitles();
 }
